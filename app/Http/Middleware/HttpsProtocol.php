2127c7c3b4b338e5a7d0b1ce335ad31b6c74a857
Vincent <vincentroeland1997@outlook.com>
2019-06-18 14:33:03 +0200

    https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -19,6 +19,7 @@ class HttpsProtocol
         
             if (!$request->secure() && App::environment() === 'production') {
                 $request->setTrustedProxies( [ $request->getClientIp() ] ); 
+                $request->server('HTTP_X_FORWARDED_PROTO') != 'https';
                 return redirect()->secure($request->getRequestUri());
             }
 

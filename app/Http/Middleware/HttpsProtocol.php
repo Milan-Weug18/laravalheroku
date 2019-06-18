5982f64700c0cecaf2821a145e94f64b6fe923a7
Vincent <vincentroeland1997@outlook.com>
2019-06-18 18:05:48 +0200

    https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -17,12 +17,13 @@ class HttpsProtocol
     public function handle($request, Closure $next)
     {
         
-         if (!$request->secure() && env('APP_ENV') === 'production') {
+         if ($request->secure() && env('APP_ENV') === 'production') {
 
+            URL::forceSchema('https');
+            return $next($request); 
+        }
             return redirect()->secure($request->getRequestUri());
 
-        }
-
-            return $next($request); 
+            
         }
 }
